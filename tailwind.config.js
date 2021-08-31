099d9f647d015087e9fb1e9d0985535e8ec1d7e9
violeine <haluudang@gmail.com>
2021-08-31 15:18:46 +0700

    lexend deca

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      sans: ["Lexend Deca", ...defaultTheme.fontFamily.sans],
     },
   },
   variants: {
